ca4c1f1e48716b561e6384da95b5e72d46160f69
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-03-18 14:40:16 +0000

    post/put update form state

--- src/app.js
+++ src/app.js
@@ -13,6 +13,10 @@ document.querySelector('#posts').addEventListener('click', deletePost);
 // Listen for edit state
 document.querySelector('#posts').addEventListener('click', enableEdit);
 
+// Listen for cancel
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
+
+
 // Get Posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
@@ -24,20 +28,41 @@ function getPosts() {
 function submitPost() {
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
+  const id = document.querySelector('#id').value;
 
   const data = {
     title,
     body
   }
 
-  // Create post
-  http.post('http://localhost:3000/posts', data)
-    .then(data => {
-      ui.showAlert('Post added', 'alert alert-success');
-      ui.clearFields();
-      getPosts();
-    })
-    .catch(err => console.log(err));
+  // Validate input
+  if(title == '' || body === '') {
+    ui.showAlert('Please fill in all fields', 'alert alert-danger');
+  } else {
+    // Check for ID
+    if(id === '') {
+      // Create post
+      http.post('http://localhost:3000/posts', data)
+      .then(data => {
+        ui.showAlert('Post added', 'alert alert-success');
+        ui.clearFields();
+        getPosts();
+      })
+      .catch(err => console.log(err));
+    } else {
+      // Update Post
+      http.put(`http://localhost:3000/posts/${id}`, data)
+      .then(data => {
+        ui.showAlert('Post updated', 'alert alert-success');
+        ui.changeFormState('add');
+        getPosts();
+      })
+      .catch(err => console.log(err));
+    }
+    
+  
+    
+  }
 }
 
 // Delete post
@@ -74,5 +99,14 @@ function enableEdit(e) {
 
     // Fill form with current post
     ui.fillForm(data);
+  }
+}
+
+// Cancel Edit State
+function cancelEdit(e) {
+  e.preventDefault();
+
+  if(e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add');
   }
 }
